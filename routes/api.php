fa12df22b3ce60a37e12768d1bcadfd772d7fb16
Artem <alexx1088@gmail.com>
2022-08-31 09:17:00 +0600

    add documentation

--- routes/api.php
+++ routes/api.php
@@ -18,25 +18,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('country', [\App\Http\Controllers\Api\Country\CountryController::class,
- 'country']);
-
-Route::get('country/{id}', [\App\Http\Controllers\Api\Country\CountryController
-::class, 'countryById']);
-
-//Route::post('country', [\App\Http\Controllers\Api\Country\CountryController::class,
-// 'countrySave']);
-
-/*Route::put('country/{id}', [\App\Http\Controllers\Api\Country\CountryController::class,
-    'countryEdit']);*/
-/*Route::delete('country/{id}', [\App\Http\Controllers\Api\Country\CountryController::class,
-    'countryDelete']);*/
-
-//Route::post('login', [\App\Http\Controllers\Api\Auth\LoginController::class, 'login']);
-
-//Route::get('refresh', [\App\Http\Controllers\Api\Auth\LoginController::class,
-// 'refresh']);
-
 
 Route::group(['middleware' => ['jwt.verify']], function () {
 
